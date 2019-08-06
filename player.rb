244cfbec046f76c69976374381f16b178e69ea40
dtrift <89174579997@mail.ru>
2019-08-06 22:53:01 +0500

    Blackjack_2_2

--- player.rb
+++ player.rb
@@ -2,6 +2,7 @@ class Player
   attr_accessor :name, :money, :current_cards, :score
 
   def initialize(name, money = 100)
+    @name = name
     @money = money
     @current_cards = []
     @score = 0
